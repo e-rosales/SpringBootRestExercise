0ae4631127e6023a7c626e4b64aa1f2e089ede86
e-rosales <enriquerospez@gmail.com>
2020-03-08 19:17:57 +0100

    Finished service layer test cases

--- springbootexercise/src/test/java/es/enrique/springbootexercise/service/AccountServiceTest.java
+++ springbootexercise/src/test/java/es/enrique/springbootexercise/service/AccountServiceTest.java
@@ -78,11 +78,11 @@ public class AccountServiceTest {
 	@Test
 	void test_createNewAccount() {
 		try {
-			String accountName = "Test 3";
+			String accountName = "Test 7";
 			String currency = "EUR";
 			service.create(accountName, currency, false);
 
-			Account created = repository.findByName("Test 3");
+			Account created = repository.findByName("Test 7");
 			assertEquals(accountName, created.getName());
 		} catch (AccountAlreadyExistsException e) {
 			fail("Account already exists.");
@@ -116,7 +116,7 @@ public class AccountServiceTest {
 		Account account = repository.findByName(name);
 		assertEquals(amount, account.getMoney().getAmount().doubleValue());
 	}
-	
+
 	/**
 	 * Tests the withdraw money operation.
 	 */
@@ -124,7 +124,7 @@ public class AccountServiceTest {
 	void test_withdrawMoney() {
 		double amount = 50.0;
 		String name = "Test 2";
-		
+
 		try {
 			service.withdraw(name, amount);
 		} catch (AccountNotFoundException e) {
@@ -135,6 +135,132 @@ public class AccountServiceTest {
 
 		Account account = repository.findByName(name);
 		assertEquals(50.0, account.getMoney().getAmount().doubleValue());
+	}
+
+	/**
+	 * Asserts that a withdrawal operation allows a treasury account to go negative.
+	 */
+	@Test
+	void test_withdrawMoney_negativeIsTreasury() {
+		try {
+			service.create("Test 3", "EUR", true);
+		} catch (AccountAlreadyExistsException e1) {
+			fail("Account already exists.");
+		}
+
+		try {
+			service.withdraw("Test 3", 20.0);
+		} catch (AccountNotFoundException e) {
+			fail("Account not found");
+		} catch (NegativeBalanceException e) {
+			fail("Negative balance");
+		}
+
+		Account account = repository.findByName("Test 3");
+		assertEquals(-20.0, account.getMoney().getAmount().doubleValue());
+	}
+
+	/**
+	 * Asserts that a withdrawal operation throws an exception when a non-treasury
+	 * account would go below 0 balance.
+	 */
+	@Test
+	void test_withdrawMoney_negativeIsNotTreasury() {
+
+		try {
+			service.create("Test 4", "EUR", false);
+
+			service.deposit("Test 4", 10.0);
+		} catch (AccountNotFoundException e) {
+			fail("Account not found");
+		} catch (AccountAlreadyExistsException e) {
+			fail("Account already exists.");
+		}
+
+		assertThrows(NegativeBalanceException.class, () -> service.withdraw("Test 4", 20.0));
+	}
+
+	/**
+	 * Asserts that a transfer money operation between two accounts is performed
+	 * successfully.
+	 */
+	@Test
+	void test_transferMoney() {
+		try {
+			String nameAccountFrom = "Test 5";
+			String nameAccountTo = "Test 6";
+			service.create(nameAccountFrom, "EUR", false);
+			service.create(nameAccountTo, "EUR", false);
+
+			service.deposit(nameAccountFrom, 100.0);
+
+			service.transfer(nameAccountFrom, nameAccountTo, 20.0);
+
+			Account accountTo = repository.findByName(nameAccountFrom);
+			Account accountFrom = repository.findByName(nameAccountTo);
+
+			assertEquals(80.0, accountTo.getMoney().getAmount().doubleValue());
+			assertEquals(20.0, accountFrom.getMoney().getAmount().doubleValue());
+
+		} catch (AccountAlreadyExistsException e) {
+			fail("Account already exists");
+		} catch (AccountNotFoundException e) {
+			fail("Account not found");
+		} catch (NegativeBalanceException e) {
+			fail("Negative balance");
+		}
+
+	}
+
+	/**
+	 * Asserts that a transfer money operation between two accounts is not performed
+	 * if the account where the money is transfered from would go negative and is
+	 * not a treasury account.
+	 */
+	@Test
+	void test_transferMoney_negativeIsNotTreasury() {
+		try {
+			String nameAccountFrom = "Test 8";
+			String nameAccountTo = "Test 9";
+			service.create(nameAccountFrom, "EUR", false);
+			service.create(nameAccountTo, "EUR", false);
+
+			assertThrows(NegativeBalanceException.class, () -> service.transfer(nameAccountFrom, nameAccountTo, 20.0));
+
+		} catch (AccountAlreadyExistsException e) {
+			fail("Account already exists");
+		}
+
+	}
+
+	/**
+	 * Asserts that a transfer money operation between two accounts is performed
+	 * allowing a treasury account to go negative.
+	 */
+	@Test
+	void test_transferMoney_negativeIsTreasury() {
+		try {
+			String nameAccountFrom = "Test 10";
+			String nameAccountTo = "Test 11";
+			service.create(nameAccountFrom, "EUR", true);
+			service.create(nameAccountTo, "EUR", false);
+
+			service.transfer(nameAccountFrom, nameAccountTo, 20.0);
+			
+			Account accountTo = repository.findByName(nameAccountFrom);
+			Account accountFrom = repository.findByName(nameAccountTo);
+
+			assertEquals(-20.0, accountTo.getMoney().getAmount().doubleValue());
+			assertEquals(20.0, accountFrom.getMoney().getAmount().doubleValue());
+			
+		} catch (AccountAlreadyExistsException e) {
+			fail("Account already exists");
+		} catch (AccountNotFoundException e) {
+			fail("Account not found");
+		} catch (NegativeBalanceException e) {
+			fail("Negative balance");
+		}
+
 	}
 
 }
